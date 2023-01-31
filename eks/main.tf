2b6273c07d8a7e61e4f07d74cffd4d1bfdcb9dde
ponomarenkomo <ponomarenko.n.2001@gmail.com>
2023-01-31 15:25:54 +0200

    oidc-provider and cert-manager-sa

--- eks/main.tf
+++ eks/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.51.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.17.0"
+    }
   }
 
   backend "s3" {
@@ -18,4 +23,16 @@ provider "aws" {
   region = var.aws_region
 
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+    command     = "aws"
+  }
+
+}
+
 
