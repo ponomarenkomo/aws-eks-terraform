38db24296e1409ebb5d96d738ade7870975fcfe2
ponomarenkomo <ponomarenko.n.2001@gmail.com>
2023-02-14 13:34:55 +0200

    security group for alb,eks

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -46,20 +46,23 @@ data "aws_availability_zones" "available" {
 resource "aws_security_group" "worker_node_sg" {
   name   = "worker_node_sg"
   vpc_id = module.vpc.vpc_id
-
-
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    to_port   = 9443
+    from_port = 9443
+    protocol  = "tcp"
+    self      = true
+  }
+  ingress {
+    to_port   = 0
+    from_port = 0
+    protocol  = "-1"
+    self      = true
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    to_port          = 0
+    from_port        = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-
-
 }
